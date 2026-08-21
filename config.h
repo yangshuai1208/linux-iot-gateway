b4020bf8fcb546a15270d35c518db19a2602984e
yangshuai1208 <431317232@qq.com>
2026-08-21 15:52:36 +0800

    feat: complete serial data source

--- config.h
+++ config.h
@@ -1,7 +1,9 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
+#ifndef USE_MOCK_DATA
 #define USE_MOCK_DATA 1
+#endif
 
 #define LOOP_COUNT 5
 
--- data_source.c
+++ data_source.c
@@ -5,7 +5,8 @@
 #include "config.h"
 #include "data_source.h"
 
-#if USE_MOCK_DATA==0
+#if USE_MOCK_DATA == 0
+#include <errno.h>
 #include <fcntl.h>
 #include <termios.h>
 #endif
@@ -51,20 +52,167 @@ void data_source_cleanup(void)
 {
  printf("data source cleanup\n");
 }
-#else 
-static int serial_fd=-1;
+#else
 
-static int serial_init(int fd)
+static int serial_fd = -1;
+
+static int serial_configure(int fd)
 {
- struct termios options;
- 
- if(tcgetattr(fd,&options)!=0)
- {
-  perror("tcgetattr failed");
-  return -1;
- }
+    struct termios options;
 
-cfsetispeed(&options,B115200);
-cfsetospeed(&options,B115200);
+    if (tcgetattr(fd, &options) != 0)
+    {
+        perror("tcgetattr failed");
+        return -1;
+    }
+
+    if (cfsetispeed(&options, B115200) != 0 ||
+        cfsetospeed(&options, B115200) != 0)
+    {
+        perror("set baud rate failed");
+        return -1;
+    }
+
+    
+    options.c_cflag &= ~(PARENB | CSTOPB | CSIZE);
+    options.c_cflag |= CS8;
+
+    
+    options.c_cflag |= CREAD | CLOCAL;
+
+#ifdef CRTSCTS
+    /* 关闭硬件流控 */
+    options.c_cflag &= ~CRTSCTS;
+#endif
+
+
+    options.c_iflag &= ~(IXON | IXOFF | IXANY);
+    options.c_iflag &= ~(IGNBRK |
+                         BRKINT |
+                         PARMRK |
+                         ISTRIP |
+                         INLCR |
+                         IGNCR |
+                         ICRNL);
+
+   
+    options.c_lflag &= ~(ICANON | ECHO | ECHOE | ISIG);
+
+  
+    options.c_oflag &= ~OPOST;
+
+  
+    options.c_cc[VMIN] = 0;
+    options.c_cc[VTIME] = 10;
+
+    if (tcflush(fd, TCIFLUSH) != 0)
+    {
+        perror("tcflush failed");
+        return -1;
+    }
+
+    if (tcsetattr(fd, TCSANOW, &options) != 0)
+    {
+        perror("tcsetattr failed");
+        return -1;
+    }
+
+    return 0;
+}
+
+int data_source_init(void)
+{
+    serial_fd = open(SERIAL_DEV, O_RDWR | O_NOCTTY);
+
+    if (serial_fd < 0)
+    {
+        perror("open serial device failed");
+        return -1;
+    }
+
+    if (serial_configure(serial_fd) != 0)
+    {
+        close(serial_fd);
+        serial_fd = -1;
+        return -1;
+    }
+
+    printf("data source: serial mode, device=%s\n", SERIAL_DEV);
+    return 0;
+}
+
+int data_source_read(char *buf, int buf_size)
+{
+    int total = 0;
+    char ch;
+
+    if (buf == NULL || buf_size <= 1 || serial_fd < 0)
+    {
+        return -1;
+    }
+
+    buf[0] = '\0';
+
+    while (total < buf_size - 1)
+    {
+        ssize_t received = read(serial_fd, &ch, 1U);
+
+        if (received > 0)
+        {
+            if (ch == '\r' || ch == '\n')
+            {
+                if (total > 0)
+                {
+                    break;
+                }
+
+                /* 忽略一帧开始前残留的换行符 */
+                continue;
+            }
+
+            buf[total] = ch;
+            ++total;
+        }
+        else if (received == 0)
+        {
+            /* 串口读取超时 */
+            break;
+        }
+        else
+        {
+            if (errno == EINTR)
+            {
+                continue;
+            }
+
+            perror("read serial device failed");
+            return -1;
+        }
+    }
+
+    buf[total] = '\0';
+
+    if (total == 0)
+    {
+        return -1;
+    }
+
+    return 0;
+}
+
+void data_source_cleanup(void)
+{
+    if (serial_fd >= 0)
+    {
+        if (close(serial_fd) != 0)
+        {
+            perror("close serial device failed");
+        }
+
+        serial_fd = -1;
+    }
+
+    printf("data source cleanup\n");
+}
 
 #endif
